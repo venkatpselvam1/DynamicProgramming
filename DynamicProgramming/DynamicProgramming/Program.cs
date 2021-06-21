167d26dd014392bf1f121cd665c0cfdec27f3e5c
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-06-21 08:20:32 +0530

    creating sln

---
+++ DynamicProgramming/DynamicProgramming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Dynamic Programming (DP) is an algorithmic technique for solving an optimization problem
+            by breaking it down into simpler subproblems and utilizing the fact that the optimal solution
+            to the overall problem depends upon the optimal solution to its subproblems.
+             */
+        }
+    }
+}
