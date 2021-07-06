991d06eee734c1bfdd903095ed2d4b25032ade2a
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-07-06 19:58:44 +0530

    refactor and move point to utility proj

--- DynamicProgramming/001_PointReachable_2D/Program.cs
+++ DynamicProgramming/001_PointReachable_2D/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities;
 
 namespace _001_PointReachable_2D
 {
@@ -29,51 +30,7 @@ namespace _001_PointReachable_2D
             };
             Console.WriteLine(IsPointReachable(arr, start, end));
         }
-        public class Point
-        {
-            public int X { get; set; }
-            public int Y { get; set; }
-            public bool Equals(Point obj)
-            {
-                return obj.X == this.X && obj.Y == this.Y;
-            }
-            public Point GetLeft()
-            {
-                return new Point() 
-                {
-                    X = this.X - 1,
-                    Y = this.Y
-                };
-            }
-            public Point GetRight()
-            {
-                return new Point()
-                {
-                    X = this.X + 1,
-                    Y = this.Y
-                };
-            }
-            public Point GetTop()
-            {
-                return new Point()
-                {
-                    X = this.X,
-                    Y = this.Y + 1
-                };
-            }
-            public Point GetBottom()
-            {
-                return new Point()
-                {
-                    X = this.X,
-                    Y = this.Y - 1
-                };
-            }
-            public bool isValid(bool[,] arr)
-            {
-                return this.X >= 0 && this.X < arr.GetLength(0) && this.Y >= 0 && this.Y < arr.GetLength(1);
-            }
-        }
+        
         public static bool IsPointReachable(bool[,] arr, Point start, Point end) 
         {
             var arrdp = new bool?[arr.GetLength(0),arr.GetLength(1)];
---
+++ DynamicProgramming/Utilities/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Equals(Point obj)
+        {
+            return obj.X == this.X && obj.Y == this.Y;
+        }
+        public Point GetLeft()
+        {
+            return new Point()
+            {
+                X = this.X - 1,
+                Y = this.Y
+            };
+        }
+        public Point GetRight()
+        {
+            return new Point()
+            {
+                X = this.X + 1,
+                Y = this.Y
+            };
+        }
+        public Point GetTop()
+        {
+            return new Point()
+            {
+                X = this.X,
+                Y = this.Y + 1
+            };
+        }
+        public Point GetBottom()
+        {
+            return new Point()
+            {
+                X = this.X,
+                Y = this.Y - 1
+            };
+        }
+        public bool isValid(bool[,] arr)
+        {
+            return this.X >= 0 && this.X < arr.GetLength(0) && this.Y >= 0 && this.Y < arr.GetLength(1);
+        }
+    }
+}
